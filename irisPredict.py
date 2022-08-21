fa51c1952957c41f586c703a5bf02ce1b9c41d32
Nimish Tiwari <nktn1605@gmail.com>
2022-08-21 21:09:44 +0530

    Create irisPredict.py

---
+++ irisPredict.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from irisSoftmax import SoftmaxReg
+
+FILE_NAME = "irisMod.data"
+data = np.genfromtxt(FILE_NAME , delimiter=",")
+
+regressor = SoftmaxReg()
+
+n_sample,n_features = data.shape
+n_features -= 1
+
+x = data[:,0:n_features]
+y = data[:,n_features]
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=20)
+
+regressor.fit(x_train,y_train,c=3)
+y_pred = regressor.predict(x_test)
+accur = regressor.accurate(y_test,y_pred)
+print(accur)
