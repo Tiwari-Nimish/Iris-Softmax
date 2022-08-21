ef8f739a29183569e959c6905f7cb9ef8a4144f5
Nimish Tiwari <nktn1605@gmail.com>
2022-08-21 21:13:52 +0530

    Update irisPredict.py

--- irisPredict.py
+++ irisPredict.py
@@ -15,7 +15,7 @@ x = data[:,0:n_features]
 y = data[:,n_features]
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=20)
 
-regressor.fit(x_train,y_train,c=3)
+regressor.fit(x_train,y_train,c=3)#'c' is the number of classes which are to be classified
 y_pred = regressor.predict(x_test)
 accur = regressor.accurate(y_test,y_pred)
 print(accur)
